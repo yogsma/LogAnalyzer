b759162f263df02acc631040469624bbff0f628f
Yogesh Mali <yogesh.mali@gmail.com>
2021-07-13 21:38:40 -0500

    Initial facade for Log Analyzer REST API

---
+++ src/main/java/com/betterjavacode/loganalyzer/LoganalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.betterjavacode.loganalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoganalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoganalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/betterjavacode/loganalyzer/LoganalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betterjavacode.loganalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoganalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
