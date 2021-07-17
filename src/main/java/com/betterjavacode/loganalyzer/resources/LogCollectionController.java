51550161ec89dd86a49ba9343ced2447d81f20c5
Yogesh Mali <yogesh.mali@gmail.com>
2021-07-17 10:58:47 -0500

    Implemention of APIs for retrieving log from a file and top N events from the specified file

--- src/main/java/com/betterjavacode/loganalyzer/resources/LogCollectionController.java
+++ src/main/java/com/betterjavacode/loganalyzer/resources/LogCollectionController.java
@@ -3,17 +3,12 @@ package com.betterjavacode.loganalyzer.resources;
 import com.betterjavacode.loganalyzer.models.LogContent;
 import com.betterjavacode.loganalyzer.service.LogCollectionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,19 +22,19 @@ public class LogCollectionController
 
     @GetMapping
     public ResponseEntity<Map<String,Object>> getLogContent(@RequestParam("fileName") String fileName,
-                                                            @RequestParam(defaultValue = "0") int page,
-                                                            @RequestParam(defaultValue = "50") int size) throws Exception
+                                                            @RequestParam(name =
+                                                                    "page", defaultValue = "0") int page,
+                                                            @RequestParam(name= "size",
+                                                                    defaultValue =
+                                                                    "100") int size) throws Exception
     {
         if(fileName == null || fileName.isEmpty())
         {
             throw new Exception("Provide a file name");
         }
-        List<LogContent> logContents = new ArrayList<>();
 
-        Pageable pageable = PageRequest.of(page, size);
-        Page<LogContent> pageLogContent;
-        pageLogContent = logCollectionService.getLogContentFromFile(pageable, fileName);
-        logContents = pageLogContent.getContent();
+        List<String> logContents = logCollectionService.getLogContentFromFile(page, size,
+                fileName);
 
         if(logContents.isEmpty())
         {
@@ -52,14 +47,29 @@ public class LogCollectionController
         return new ResponseEntity<>(responseObject, HttpStatus.OK);
     }
 
-    @GetMapping
-    public List<LogContent> getTopLogContent(@RequestParam("fileName") String fileName)
+    @GetMapping("/top")
+    public ResponseEntity<Map<String,Object>> getTopLogContent(@RequestParam("fileName") String fileName, @RequestParam("top")int topN) throws Exception
     {
-        return null;
+        if(fileName == null || fileName.isEmpty())
+        {
+            throw new Exception("Provide a file name");
+        }
+
+        List<String> logContents = logCollectionService.getLogContentFromFile(fileName, topN);
+
+        if(logContents.isEmpty())
+        {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        Map<String, Object> responseObject = new HashMap<>();
+        responseObject.put("logcontent", logContents);
+
+        return new ResponseEntity<>(responseObject, HttpStatus.OK);
     }
 
-    @GetMapping
-    public List<LogContent> searchLogContent(@RequestParam("keyword") String keyword)
+    @GetMapping("/search")
+    public List<String> searchLogContent(@RequestParam("keyword") String keyword)
     {
         return null;
     }
--- src/main/java/com/betterjavacode/loganalyzer/service/LogCollectionService.java
+++ src/main/java/com/betterjavacode/loganalyzer/service/LogCollectionService.java
@@ -1,17 +1,83 @@
 package com.betterjavacode.loganalyzer.service;
 
-import com.betterjavacode.loganalyzer.models.LogContent;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Service
 public class LogCollectionService
 {
+    private static final Logger logger = LoggerFactory.getLogger(LogCollectionService.class);
 
-    public Page<LogContent> getLogContentFromFile (Pageable pageable, String fileName)
+    public List<String> getLogContentFromFile (int pageNumber, int pageSize,
+                                                   String fileName)
     {
-        // TO-DO
+        File file = findFileFromDirectory(fileName);
+
+
+        if(file != null)
+        {
+            try
+            {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                List<String> list = new ArrayList<>();
+                for(int i = pageNumber * pageSize; i < (1 + pageNumber) * pageSize; i++)
+                {
+                    String message = bufferedReader.readLine();
+                    list.add(message);
+                }
+                bufferedReader.close();
+                return list;
+            }
+            catch(IOException e)
+            {
+                logger.error("Error accessing file ", e);
+            }
+        }
+        return new ArrayList<>();
+    }
+
+
+    private File findFileFromDirectory(String fileName)
+    {
+        File[] files = new File("C://var//log").listFiles();
+        if(files != null)
+        {
+            return getFile(fileName, files);
+        }
         return null;
+    }
+
+    private File getFile(String fileName, File[] files)
+    {
+        for(File file : files)
+        {
+            if(file.isDirectory())
+            {
+                getFile(fileName, file.listFiles());
+            }
+            else
+            {
+                if(fileName.equals(file.getName()))
+                {
+                    return file;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public List<String> getLogContentFromFile (String fileName, int topN)
+    {
+        return getLogContentFromFile(0, topN, fileName);
     }
 }
